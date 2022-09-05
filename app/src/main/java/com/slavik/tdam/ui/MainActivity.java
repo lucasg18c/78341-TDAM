a66b671f688cbbda61c61f5f88340f1bd30e5cae
Lucas Slavik <lucasg18c@gmail.com>
2022-09-05 01:14:18 -0300

    monitorear estado de la conexion

--- app/src/main/java/com/slavik/tdam/ui/MainActivity.java
+++ app/src/main/java/com/slavik/tdam/ui/MainActivity.java
@@ -1,6 +1,15 @@
 package com.slavik.tdam.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
@@ -21,16 +30,29 @@ public class MainActivity extends AppCompatActivity {
     private IRepository repository;
     private Photoset currentPhotoset;
     private Photo currentPhoto;
+    private TextView lblNoConection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lblNoConection = findViewById(R.id.lblNoConection);
+
+//        new NetworkChangeReceiver().observe(state -> {
+//
+//            Toast.makeText(
+//                            MainActivity.this,
+//                            "La conexión cambió a " + state,
+//                            Toast.LENGTH_LONG)
+//                    .show();
+//        });
+
+
         DatabaseTDAM db = Room.databaseBuilder(
-                getApplicationContext(),
-                DatabaseTDAM.class,
-                "db-tdam")
+                        getApplicationContext(),
+                        DatabaseTDAM.class,
+                        "db-tdam")
                 .allowMainThreadQueries()
                 .build();
 
@@ -44,6 +66,33 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container_view, HomeFragment.class, null)
                     .commit();
         }
+    }
+
+    private final BroadcastReceiver networkChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager
+                    = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo activeNetworkInfo = connectivityManager != null ? connectivityManager.getActiveNetworkInfo() : null;
+            boolean connected = activeNetworkInfo != null && activeNetworkInfo.isConnected();
+            lblNoConection.setVisibility(connected ? View.GONE : View.VISIBLE);
+        }
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        unregisterReceiver(networkChangeReceiver);
     }
 
     public IRepository getRepository() {
